24d1b3594017c2a4ed054b825dbaa40489c17b51
Ansh <anshthakare542@gmail.com>
2026-04-11 16:29:42 +0530

    Use truckproject-8e5e8 Firebase configuration in firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,30 +1,29 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       return const FirebaseOptions(
-        apiKey: "AIzaSyC587I5tZQXGCDawJDJoAumLOnkzV_OVug",
-        authDomain: "womensafetyapp-990d4.firebaseapp.com",
-        projectId: "womensafetyapp-990d4",
-        storageBucket: "womensafetyapp-990d4.firebasestorage.app",
-        messagingSenderId: "4890750983",
-        appId: "1:4890750983:web:b0ca260a5987f28a0b3aa2",
-        measurementId: "G-4LTKQZDB1V",
+        apiKey: "AIzaSyAp9_qurmvthIIxy4_Hi_VoKRt5snfkZ8k",
+        authDomain: "truckproject-8e5e8.firebaseapp.com",
+        projectId: "truckproject-8e5e8",
+        storageBucket: "truckproject-8e5e8.firebasestorage.app",
+        messagingSenderId: "144627799852",
+        appId: "1:144627799852:web:88e567ca85add489c27e89",
+        measurementId: "G-PE2CSPTH9D",
       );
     }
 
     // Fallback for other platforms (mobile app)
     return const FirebaseOptions(
-      apiKey: "AIzaSyC587I5tZQXGCDawJDJoAumLOnkzV_OVug",
-      authDomain: "womensafetyapp-990d4.firebaseapp.com",
-      projectId: "womensafetyapp-990d4",
-      storageBucket: "womensafetyapp-990d4.firebasestorage.app",
-      messagingSenderId: "4890750983",
-      appId: "1:4890750983:web:b0ca260a5987f28a0b3aa2",
-      measurementId: "G-4LTKQZDB1V",
+      apiKey: "AIzaSyAp9_qurmvthIIxy4_Hi_VoKRt5snfkZ8k",
+      authDomain: "truckproject-8e5e8.firebaseapp.com",
+      projectId: "truckproject-8e5e8",
+      storageBucket: "truckproject-8e5e8.firebasestorage.app",
+      messagingSenderId: "144627799852",
+      appId: "1:144627799852:web:88e567ca85add489c27e89",
+      measurementId: "G-PE2CSPTH9D",
     );
   }
 }
